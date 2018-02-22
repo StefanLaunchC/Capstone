2f3c8cea1abf9a54ec6b66de9b55bce1f4d59a58
Stefan <ste_fan_work@yahoo.com>
2018-02-21 19:23:42 -0600

    view changes 2/21/2018

--- Capstone/Controllers/HomeController.cs
+++ Capstone/Controllers/HomeController.cs
@@ -6,27 +6,27 @@ namespace Capstone.Controllers
     {
         public IActionResult Index()
         {
-            return Content("<h1>Bill & Mike Law Firm</h1>", "text/html");
+            return View(); //Content("<h1>Bill & Mike Law Firm</h1>", "text/html");
         }
         [Route("/About")]
         public IActionResult About()
-        { 
-            return Content("<h1>This is Bill, This is Mike</h1>", "text/html");
+        {
+            return View(); //Content("<h1>This is Bill, This is Mike</h1>", "text/html");
         }
         [Route("/Contact")]
         public IActionResult Contact()
         {
-            return Content("<h1>This is where Bill and Mikes office is located</h1>", "text/html");
+            return View(); // Content("<h1>This is where Bill and Mikes office is located</h1>", "text/html");
         }
         [Route("/ClientInfo")]
         public IActionResult ClientInfo()
         {
-            return Content("<h1>Helpful information for clients</h1>", "text/html");
+            return View();  //Content("<h1>Helpful information for clients</h1>", "text/html");
         }
         [Route("/PublicInfo")]
         public IActionResult PublicInfo()
         {
-            return Content("<h1>Headings</h1>", "text/html");
+            return View();  //Content("<h1>Headings</h1>", "text/html");
         }
     }
 }
