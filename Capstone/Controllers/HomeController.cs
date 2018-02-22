652b54d3cda184c837ef9c5364e46702c30501a9
Stefan <ste_fan_work@yahoo.com>
2018-02-21 18:48:49 -0600

    IActionResult 2/21/2018

--- Capstone/Controllers/HomeController.cs
+++ Capstone/Controllers/HomeController.cs
@@ -8,5 +8,25 @@ namespace Capstone.Controllers
         {
             return Content("<h1>Bill & Mike Law Firm</h1>", "text/html");
         }
+        [Route("/About")]
+        public IActionResult About()
+        { 
+            return Content("<h1>This is Bill, This is Mike</h1>", "text/html");
+        }
+        [Route("/Contact")]
+        public IActionResult Contact()
+        {
+            return Content("<h1>This is where Bill and Mikes office is located</h1>", "text/html");
+        }
+        [Route("/ClientInfo")]
+        public IActionResult ClientInfo()
+        {
+            return Content("<h1>Helpful information for clients</h1>", "text/html");
+        }
+        [Route("/PublicInfo")]
+        public IActionResult PublicInfo()
+        {
+            return Content("<h1>Headings</h1>", "text/html");
+        }
     }
 }
